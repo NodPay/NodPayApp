0a606e7fd6537188a531e88f87eb351bb12ef236
andi.rayka <andi.rayka@gmail.com>
2021-07-10 10:58:52 +0700

    add eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    'react/self-closing-comp': [
-      'error',
-      {
-        component: true,
-        html: true,
-      },
-    ],
     'prettier/prettier': ['error'],
+    'react-native/no-inline-styles': 'off',
   },
 };
